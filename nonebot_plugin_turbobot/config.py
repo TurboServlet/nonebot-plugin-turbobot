8fe561ac0f653101e3068f68bfa6759e26a47b30
YuuLuo <69673808+YuuLuo@users.noreply.github.com>
2024-07-16 12:39:53 +0800

    Update config.py

--- nonebot_plugin_turbobot/config.py
+++ nonebot_plugin_turbobot/config.py
@@ -1,6 +1,6 @@
 from pydantic import BaseSettings, Field, validator
 
 class Config(BaseSettings):
-    database_path: str = Field(default='src/plugins/turbo/database/botKey.db')
+    database_path: str = Field(default='src/plugins/nonebot_plugin_turbobot/database/botKey.db')
     api_base_url: str = Field(default='https://api.mai-turbo.net')
     bot_name: str = Field(default="Salt二号机")
