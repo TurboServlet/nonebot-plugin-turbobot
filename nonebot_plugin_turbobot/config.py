830cbf6bee4cf74b8ea4715745488438a55c94b8
澪度 <117048039+lingdu2333@users.noreply.github.com>
2025-11-15 17:55:17 +0800

    fix: 修复: 将 BaseSettings 迁移到 pydantic-settings 以兼容 Pydantic v2
    Pydantic v2 已将 BaseSettings 移动到 pydantic-settings 包
    
    原来的import方式会报错：
    `pydantic.errors.PydanticImportError: BaseSettings has been moved to the pydantic-settings package`

--- nonebot_plugin_turbobot/config.py
+++ nonebot_plugin_turbobot/config.py
@@ -1,6 +1,7 @@
-from pydantic import BaseSettings, Field, validator
+from pydantic_settings import BaseSettings
+from pydantic import Field, field_validator
 
 class Config(BaseSettings):
     database_path: str = Field(default='src/plugins/nonebot_plugin_turbobot/database/botKey.db')
     api_base_url: str = Field(default='https://api.mai-turbo.net')
-    bot_name: str = Field(default="Salt二号机")
+    bot_name: str = Field(default="Turbobot")
